c337b6eb087df69b8e6b18b88b0b98c5420f49aa
lisi <lisi@qq.com>
2021-12-14 08:28:34 +0800

    second commit

--- src/main/java/com/exer/git/GitTest.java
+++ src/main/java/com/exer/git/GitTest.java
@@ -6,4 +6,8 @@ package com.exer.git;
  * @date 2021/12/13
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello  git");
+        System.out.println("hello");
+    }
 }
